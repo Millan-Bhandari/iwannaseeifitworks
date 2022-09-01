71dfd410587c3f4eaa47805e98de64d93ce68676
Millan Bhandari <109762935+Millan-Bhandari@users.noreply.github.com>
2022-08-31 20:25:21 -0500

    Initial Commit

---
+++ iwannaseeifitworks/iwannaseeifitworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iwannaseeifitworksApp.swift
+//  iwannaseeifitworks
+//
+//  Created by Millan Bhandari on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct iwannaseeifitworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
